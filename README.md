3dd76f82974af69701fdd15ae5dc81d97e1b68e8
Hasan <hasan98209@gmail.com>
2024-01-24 20:09:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Django5- Book-Store
+ Django5 Book Store Project
---
+++ book_outlet/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, get_object_or_404
+from django.db.models import Avg
+from .models import Book
+
+# Create your views here.
+def index(request):
+  books = Book.objects.all()
+  num_books = books.count()
+  avg_rating = books.aggregate(Avg("rating"))
+  return render(request,"book_outlet/index.html", {"books":books,
+    "total_number_of_books":num_books,
+    "average_rating":avg_rating,                                                                                              })
+
+def book_detail(request,slug):
+  book = get_object_or_404(Book,slug=slug)
+  book = Book.objects.get(pk=id)
+  return render(request,"book_outlet/book_detail.html",{
+    "title": book.title,
+    "author":book.author,
+    "rating":book.rating,
+    "is_bestselling":book.is_bestselling
+  })
