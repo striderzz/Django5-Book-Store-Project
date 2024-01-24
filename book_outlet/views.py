182b411f62a042a049c0211e3a39ad84135501e7
Hasan <hasan98209@gmail.com>
2024-01-24 20:15:02 +0530

    initial commit

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -4,7 +4,7 @@ from .models import Book
 
 # Create your views here.
 def index(request):
-  books = Book.objects.all()
+  books = Book.objects.all().order_by("title")
   num_books = books.count()
   avg_rating = books.aggregate(Avg("rating"))
   return render(request,"book_outlet/index.html", {"books":books,
